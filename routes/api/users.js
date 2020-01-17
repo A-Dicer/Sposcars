f78ac55598b77df3b9c8fa11d92fe385447b9872
Andrew Dicer <andrew.dicer@gmail.com>
2020-01-17 15:00:08 -0600

    heroku

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
+
 //------------- Matches with "/api/users" --------------------
 router.route("/").get(usersController.findAll)
 router.route("/:id").get(usersController.findById).put(usersController.update)

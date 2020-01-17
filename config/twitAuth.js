6820c24c31f5624cccfb78465525c9c64f431855
Andrew Dicer <andrew.dicer@gmail.com>
2020-01-17 17:41:48 -0600

    heroku

--- config/twitAuth.js
+++ config/twitAuth.js
@@ -3,6 +3,6 @@ module.exports =  {
         'consumer_secret': 'BGIhnnyxkzYUZAmkHsUnmF6uk1uL4AHHDT5qe5F8KxDI7OmMOG',
         'access_token_key': '885224216005685251-AkM5PIbMEjFR1Cg7n6vO2j6IEFqJHSC',
         'access_token_secret': 'i3AM2PmZ1XyQGK6zxywwJJ2EpJMDWR16McS9KzpaW481D',
-        'callbackURL': 'https://sposcars.herokuapp.com/api/auth/twitter/callback',
-        'redirect': 'https://sposcars.herokuapp.com/Main'
+        'callbackURL': '/api/auth/twitter/callback',
+        'redirect': '/Main'
 };
